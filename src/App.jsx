ed924ee6b28f0132559f0a5410cd4d9d0723d23c
nubsuki <kavinduherath.14@gmail.com>
2025-09-28 16:31:43 +0530

    Add @vercel/analytics dependency and integrate into main application

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { FFmpegProvider } from './contexts/FFmpegContext'
-import FFmpegLoader from './components/FFmpegLoader'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import VideoSplitter from './pages/VideoSplitter'
-import AudioExtractor from './pages/AudioExtractor'
-import VideoConverter from './pages/VideoConverter'
-import AudioConverter from './pages/AudioConverter'
-import AudioSplitter from './pages/AudioSplitter'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { FFmpegProvider } from './contexts/FFmpegContext';
+import FFmpegLoader from './components/FFmpegLoader';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import VideoSplitter from './pages/VideoSplitter';
+import AudioExtractor from './pages/AudioExtractor';
+import VideoConverter from './pages/VideoConverter';
+import AudioConverter from './pages/AudioConverter';
+import AudioSplitter from './pages/AudioSplitter';
+import './App.css';
 
 function AppContent() {
   return (
